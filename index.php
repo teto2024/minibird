66596fa62a6ccf0559c5da880f9b9cb939723afb
teto2024 <166979361+teto2024@users.noreply.github.com>
2025-12-22 19:47:23 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -159,9 +159,7 @@ document.querySelectorAll('.buff-icon').forEach(function(el){
     <a href="quests.php" class="tabBtn block">クエスト</a>
     <a href="vip_shop.php" class="tabBtn block">VIPショップ</a>
     <a href="how_to.php" class="tabBtn block">使い方</a>
-    <a href="admin.php" class="tabBtn block">管理者/MOD用</a>
-    <a href="admin_password_reset.php" class="tabBtn block">管理者用</a>
-    <a href="frame_admin.php" class="tabBtn block">管理者用2</a>
+    <a href="admin_unified.php" class="tabBtn block">管理画面統合版</a>
     <!--<a href="darkroom.php" class="tabBtn block">暗い部屋</a>-->
     <a href="trends.php" class="tabBtn block">トレンド</a>
   <?php endif; ?>
